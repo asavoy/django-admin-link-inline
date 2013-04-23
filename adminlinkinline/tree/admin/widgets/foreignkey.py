370640624e86ed919b6f0e66b3fff7eff40dda09
Peter Schmidt <pschmidt@mathspace.com.au>
2013-04-23 19:23:58 +1000

    … mostly to make it clearer that this is dead code.

--- adminlinkinline/tree/admin/widgets/foreignkey.py
+++ adminlinkinline/tree/admin/widgets/foreignkey.py
@@ -4,7 +4,6 @@ with related items.
 """
 from django import forms
 from django.core import urlresolvers
-from django.utils.encoding import force_unicode
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 
@@ -29,11 +28,6 @@ class RenderLink(forms.Widget):
         url_pattern = '%s:%s_%s_change' % ('admin', app_label, modelname)
         url = urlresolvers.reverse(url_pattern, args=[value])
 
-        if value is None: value = ''
-        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
-        if value != '':
-            # Only add the 'value' attribute if a value is non-empty.
-            final_attrs['value'] = force_unicode(value)
         return mark_safe(
             '<a href="%s">%s</a><br>' % (
             escape(url), escape(label)
